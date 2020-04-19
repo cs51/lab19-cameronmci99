1418432cda271a786b556c9ff5ea1aec815460fb
Cameron <cameron_mcinroy@college.harvard.edu>
2020-04-19 16:39:55 -0700

    here

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -13,6 +13,7 @@ Another reasonable alternative design would be to encapsulate all ofthe database
 open Printf ;;
 open Scanf ;;(* Make use of an account database *)
 
+module DB = Database ;; (* Customer account identifiers *)
 
 type id = int ;;
 (* Possible actions that an ATM customer can perform *)
@@ -32,14 +33,14 @@ ATM component behaviors. See .mli file for further documentation. *)
 
 let initialize (initial : account_spec list) : unit =  initial
   |> List.iter (fun {name; id; balance}
-  -> Database.create id name;
-  Database.update id balance) ;;
+  -> DB.create id name;
+  DB.update id balance) ;;
 
   let rec acquire_id () : id =
   printf "Enter customer id: ";
   try
   let id = read_int () in
-  ignore (Database.exists id); id  with
+  ignore (DB.exists id); id  with
   | Not_found
   | Failure _ -> printf "Invalid id \n";
   acquire_id () ;;
@@ -48,13 +49,4 @@ let initialize (initial : account_spec list) : unit =  initial
   amount  with  | Failure _ -> printf "Invalid amount \n";                 acquire_amount () ;;
 
   let rec acquire_act () : action =  printf "Enter action: (B) Balance (-) Withdraw (+) Deposit \          (=) Done (X) Exit: %!";  scanf " %c"
-  (fun char -> match char with                     | 'b' | 'B'        -> Balance                     | '/' | 'x' | 'X'  -> Finished
-    | '='              -> Next                     | 'w' | 'W' | '-'  -> Withdraw (acquire_amount ())                     | 'd' | 'D' | '+'  -> Deposit (acquire_amount ())
-    | _                -> printf "  invalid choice\n";                                           acquire_act () ) ;;
-
-  let get_balance : id -> int = DB.balance ;;let get_name : id -> string = DB.name ;;
-
-  let update_balance : id -> int -> unit = DB.update ;;let present_message (msg : string) : unit =   printf "%s\n%!" msg ;;let deliver_cash (amount : int) : unit =  printf "Here's your cash: ";
-
-  (* dispense some "20's" *)  for _i = 1 to (amount / 20) do    printf "[20 @ 20]"  done;
-  (* dispense the rest of the cash *)  printf " and %d more\n" (amount mod 20) ;;
+  (fun char -> match char with                     | 'b' | 'B'        -> Balance                     | '/' | 'x' | 'X'  -> Finished                     | '='              -> Next                     | 'w' | 'W' | '-'  -> Withdraw (acquire_amount ())                     | 'd' | 'D' | '+'  -> Deposit (acquire_amount ())                     | _                -> printf "  invalid choice\n";                                           acquire_act () ) ;;let get_balance : id -> int = DB.balance ;;let get_name : id -> string = DB.name ;;let update_balance : id -> int -> unit = DB.update ;;let present_message (msg : string) : unit =   printf "%s\n%!" msg ;;let deliver_cash (amount : int) : unit =  printf "Here's your cash: ";  (* dispense some "20's" *)  for _i = 1 to (amount / 20) do    printf "[20 @ 20]"  done;  (* dispense the rest of the cash *)  printf " and %d more\n" (amount mod 20) ;;
